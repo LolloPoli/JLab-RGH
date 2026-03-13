88dec65102b0e68f64d8ce060fee130c088700f2
Lorenzo Polizzi <149718274+LolloPoli@users.noreply.github.com>
2026-03-13 10:07:35 +0100

    Add files via upload

---
+++ rgh_productions/jobs/rgc_mc/submit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=rgh_simulation
+#SBATCH --output=/farm_out/%u/%x-%j-%N.out
+#SBATCH --error=/farm_out/%u/%x-%j-%N.err
+#SBATCH --account=clas12
+#SBATCH --partition=production
+#SBATCH --mem-per-cpu=2000
+#SBATCH --gres=disk:5000
+#SBATCH -c 1
+#SBATCH --time=24:00:00
+#SBATCH --mail-user=%u@jlab.org
+
+$RGH_SIM_HOME/job.sh
